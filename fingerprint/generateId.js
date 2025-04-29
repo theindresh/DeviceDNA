662853a5d60f39cf8acda892156791f1822a477b
INDRESH BHARATI <indreshbharati77@gmail.com>
2025-04-30 00:59:51 +0530

    update version

--- fingerprint/generateId.js
+++ fingerprint/generateId.js
@@ -1,12 +1,12 @@
 import { hashData } from './hashData.js';
 
-export const generateDeviceId = async (fingerprintData) => {
+export const generateDeviceId = async (fingerprintData, getFingerprintData) => {
   // Sort object keys and stringify to ensure consistent input
   const stableData = Object.keys(fingerprintData).sort().reduce((acc, key) => {
     acc[key] = fingerprintData[key];
     return acc;
   }, {});
   
-  const inputString = JSON.stringify(stableData); // Always stringify the data to ensure it's stable
+  const inputString = JSON.stringify(stableData) + getFingerprintData // Always stringify the data to ensure it's stable
   return await hashData(inputString); // Generate the hash for the consistent fingerprint
 };
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import { collectFingerprintData } from './fingerprint/collectData.js';
 import { generateDeviceId } from './fingerprint/generateId.js';
+import { getFingerprint } from '@thumbmarkjs/thumbmarkjs';
 
 export const getDeviceFingerprint = async () => {
   const fingerprintData = await collectFingerprintData();
-  const deviceId = await generateDeviceId(fingerprintData);
+  const grtFingerprintData = await getFingerprint();
+
+  const deviceId = await generateDeviceId(fingerprintData, grtFingerprintData);
   
   return {
     deviceId,
